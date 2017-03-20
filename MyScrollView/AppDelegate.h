acc37ae23007f48804025626f749d605133b17a4
David Guichon <davidrguichon@gmail.com>
2017-03-20 10:15:18 -0400

    Initial Commit

---
+++ MyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyScrollView
+//
+//  Created by David Guichon on 2017-03-20.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
